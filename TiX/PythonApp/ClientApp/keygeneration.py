1ad2b91fc8bfb1736a48e106a9957aaa6cc28edf
Alan Karpovsky <akarpovs@alu.itba.edu.ar>
2013-09-01 20:22:43 -0300

    Firma del mensaje a enviar!

--- TiX/PythonApp/ClientApp/keygeneration.py
+++ TiX/PythonApp/ClientApp/keygeneration.py
@@ -1,4 +1,6 @@
 from Crypto.PublicKey import RSA
+from Crypto import Random
+
 
 def generateKeyPair(privateKeyFile, publicKeyFile):
 	keys = RSA.generate(1024)
@@ -14,3 +16,22 @@ def generateKeyPair(privateKeyFile, publicKeyFile):
 	pubHandle.close()
 
 	return exportableKeys[1]
+
+if __name__ == "__main__":
+
+	generateKeyPair("tix_key.priv", "tix_key.pub")
+	privateKeyFile = open('tix_key.priv','r')
+	publicKeyFile = open('tix_key.pub','r')
+	privateKey = RSA.importKey(privateKeyFile.read())
+	publicKey = RSA.importKey(publicKeyFile.read())
+	msg = "hola como estas"
+	signedMessage = privateKey.sign(msg, Random.new().read) #El cliente firma el msg
+	print signedMessage
+
+	publicKeyPlain = publicKey.exportKey()
+	#Cliente envia al server: DATA|publicKeyPlain|signedMeessage|msg
+
+	## En el server, valido el 'DATA' y con la signature y el msg lo verifico. 
+	## Por ultimo uso la publicKeyPlain para buscar el usuario
+
+	print publicKey.verify(msg, signedMessage) # En el servidor se hace el VERIFY, para esto se necesita tambien la firma!
