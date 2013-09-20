691af1bd3306f192d329b70e2a6a4bec8f34a358
Alan Karpovsky <akarpovs@alu.itba.edu.ar>
2013-09-20 10:12:17 -0300

    Commit para merge

--- TiX/PythonApp/ClientApp/udpClientTiempos.py
+++ TiX/PythonApp/ClientApp/udpClientTiempos.py
@@ -53,7 +53,7 @@ def relleno_largo(largo, check, told,log_file):
 		sign = signer.sign(digest) 
 		signedMessage = privateKey.sign(msg, Random.new().read)
 		publicKeyPlain = publicKey.exportKey()
-		relleno = "DATA;;" + str(publicKeyPlain) + ";;" + base64.b64encode(sign) + ";;" + filereg.name + ";;" + base64.b64encode(msg) + ";;"
+		relleno = "DATA;;" + base64.b64encode(str(publicKeyPlain)) + ";;" + base64.b64encode(sign) + ";;" + filereg.name + ";;" + base64.b64encode(msg) + ";;"
 		for i in range(len(relleno),largo-1):
 	 		relleno= relleno + str(random.randint(0,9))
 		#print relleno
