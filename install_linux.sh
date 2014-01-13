5b943e3f7d4aef22aaa6f7ce2302bb660c52aa65
Chino <joseignaciosg9@gmail.com>
2014-01-13 20:45:52 -0300

    arreglando login

--- install_linux.sh
+++ install_linux.sh
@@ -7,34 +7,36 @@ gksudo echo
 echo "### TIX instalation script is running ###"
 
 echo "### installing python... ###"
-sudo apt-get install -y python-software-properties 2>&1 >> $TEMP_FILE
+sudo apt-get install -y python-software-properties >> $TEMP_FILE 2>&1
 
 echo "### installing kivy... ###"
-sudo add-apt-repository ppa:kivy-team/kivy 2>&1 >> $TEMP_FILE
-sudo apt-get update 2>&1 >> $TEMP_FILE
-sudo apt-get install -y python-kivy 2>&1 >> $TEMP_FILE
+sudo add-apt-repository ppa:kivy-team/kivy >> $TEMP_FILE 2>&1
+sudo apt-get update >> $TEMP_FILE 2>&1
+sudo apt-get install -y python-kivy >> $TEMP_FILE 2>&1
 
 echo "### installing pip... ###"
-sudo apt-get install -y python-pip 2>&1 >> $TEMP_FILE
+sudo apt-get install -y python-pip >> $TEMP_FILE 2>&1
 
 echo "### installing pip dependencies.. ###"
-pip install -r dependencies.txt 2>&1 >> $TEMP_FILE
+pip install -r dependencies.txt >> $TEMP_FILE 2>&1
 
 #echo "### installing iphython... ###"
-#sudo easy_install ipython 2>&1 >> $TEMP_FILE
+#sudo easy_install ipython >> $TEMP_FILE 2>&1
 
 echo "### installing postgresql...###"
-sudo apt-get install -y postgresql 2>&1 >> $TEMP_FILE
-sudo apt-get install -y postgresql-server-dev-9.1 2>&1 >> $TEMP_FILE
+sudo apt-get install -y postgresql >> $TEMP_FILE 2>&1
+sudo apt-get install -y postgresql-server-dev-9.1 >> $TEMP_FILE 2>&1
 
 echo "### intalling psycopg2.... ###"
-sudo apt-get install -y python-psycopg2 2>&1 >> $TEMP_FILE
+sudo apt-get install -y python-psycopg2 >> $TEMP_FILE 2>&1
 
 echo "### installing rsa 3.1.2... ###"
-sudo  easy_install rsa 2>&1 >> $TEMP_FILE
+sudo  easy_install rsa >> $TEMP_FILE 2>&1
 
 echo "### installing requests module... ###"
-sudo easy_install requests 2>&1 >> $TEMP_FILE
+sudo easy_install requests >> $TEMP_FILE 2>&1
 
 echo "### Launching Installation... ###"
-sudo python TiX/PythonApp/ClientApp/TixApp.py 2>&1 >> $TEMP_FILE
+cd TiX/PythonApp/ClientApp/
+sudo python TixApp.py >> $TEMP_FILE 2>&1
+cd ../../..
