4f32d2db5d74a9233da49956f5b7dde0ee54c6a5
Chino <joseignaciosg9@gmail.com>
2014-01-18 14:20:16 -0300

    removing unnecessary things

--- install_linux.sh
+++ install_linux.sh
@@ -26,11 +26,11 @@ pip install -r dependencies.txt >> $TEMP_FILE 2>&1
 #sudo easy_install ipython >> $TEMP_FILE 2>&1
 
 echo "### installing postgresql...###"
-sudo apt-get install -y postgresql >> $TEMP_FILE 2>&1
-sudo apt-get install -y postgresql-server-dev-9.1 >> $TEMP_FILE 2>&1
+#sudo apt-get install -y postgresql >> $TEMP_FILE 2>&1
+#sudo apt-get install -y postgresql-server-dev-9.1 >> $TEMP_FILE 2>&1
 
 echo "### intalling psycopg2.... ###"
-sudo apt-get install -y python-psycopg2 >> $TEMP_FILE 2>&1
+#sudo apt-get install -y python-psycopg2 >> $TEMP_FILE 2>&1
 
 echo "### installing rsa 3.1.2... ###"
 sudo  easy_install rsa >> $TEMP_FILE 2>&1
