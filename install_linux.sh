10abea9d6c67b355db5e2e9156848e717ecadfbd
Chino <joseignaciosg9@gmail.com>
2014-01-18 14:40:23 -0300

    adding git to script

--- install_linux.sh
+++ install_linux.sh
@@ -28,6 +28,9 @@ sudo  easy_install rsa >> $TEMP_FILE 2>&1
 echo "### installing requests module... ###"
 sudo easy_install requests >> $TEMP_FILE 2>&1
 
+echo "### installing git... ###"
+sudo apt-get install git-core
+
 echo "### Launching Installation... ###"
 cd TiX/PythonApp/ClientApp/
 sudo python TixApp.py >> $TEMP_FILE 2>&1
