0ffabd2dcd530a2604afd4bd5de5ebd97757bdd6
Chino <joseignaciosg9@gmail.com>
2014-02-03 19:08:04 -0300

    install script osx, run after package_osx.sh

---
+++ install_osx.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# NOTE: Install kivy!
+
+sudo easy_install pycrypto
+sudo pip install pyasn1==0.1.7
+sudo pip install wsgiref==0.1.2
+sudo pip install pyinstaller
+sudo pip install requests
+sudo pip install rsa
