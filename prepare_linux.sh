da49ada9f7e9848c2557d1ecd74e5861d1eb7cef
Cristian Pereyra <cristian@redmintlabs.com>
2014-02-19 19:30:13 -0300

    adds srcipts for linux

--- prepare_linux.sh
+++ prepare_linux.sh
@@ -4,7 +4,7 @@ TEMP_FILE=install.log
 
 gksudo echo
 
-echo "### TIX instalation script is running ###"
+echo "### TIX package environment is installing itself ###"
 
 echo "### installing python... ###"
 sudo apt-get install -y python-software-properties >> $TEMP_FILE 2>&1
@@ -28,15 +28,3 @@ sudo  easy_install rsa >> $TEMP_FILE 2>&1
 echo "### installing requests module... ###"
 sudo easy_install requests >> $TEMP_FILE 2>&1
 
-echo "### installing git... ###"
-sudo apt-get install -y git-core 2>&1
-
-echo "### registering update script ###"
-sudo cp update_linux.sh /etc/init.d/update_linux.sh 2>&1
-sudo chmod +x update_linux.sh
-sudo chmod +x /etc/init.d/update_linux.sh  2>&1
-
-echo "### Launching Installation... ###"
-cd TiX/PythonApp/ClientApp/
-sudo python TixApp.py >> $TEMP_FILE 2>&1
-cd ../../..
